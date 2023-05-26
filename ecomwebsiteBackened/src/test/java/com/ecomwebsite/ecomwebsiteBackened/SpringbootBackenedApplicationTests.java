b4567078364da64233779978e75e7e3112e94fd8
Nancy Gupta <nancy_gupta@persistent.com>
2023-05-26 14:54:53 +0530

    ecom backend added

---
+++ ecomwebsiteBackened/src/test/java/com/ecomwebsite/ecomwebsiteBackened/SpringbootBackenedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomwebsite.ecomwebsiteBackened;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootBackenedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
